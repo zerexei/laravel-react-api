80d4f098473264954bfeda5bf83df0643bd67fb1
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-09-24 13:45:40 +0800

    react useEffect

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+
 export default function App() {
-    return (
-        <div className="">
-            <h2>Hello Reactjs</h2>
-        </div>
-    );
+    let [user, setUser] = useState({});
+
+    useEffect(() => {
+        getUser();
+        console.log(user);
+    }, []);
+
+    async function getUser() {
+        const res = await fetch("/api/test");
+        user = setUser(await res.json());
+    }
+
+    return <div>User: {user.name}</div>;
 }
 const container = document.getElementById("App");
 const root = createRoot(container);
